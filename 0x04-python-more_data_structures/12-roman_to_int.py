62888faddfea952acdcb97e11507fcde1ee6e0ac
edumabagraham <edumabagrahamp@gmail.com>
2022-12-09 21:19:31 +0000

    Set and dictionary

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    roman_dict = {
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000
+    }
+    if len(roman_string) == 1:
+        return roman_dict[roman_string]
+
+    roman_numeral = 0
+    for i in roman_string:
+        roman_numeral += roman_dict[i]
+    return roman_numeral 
+# touc
