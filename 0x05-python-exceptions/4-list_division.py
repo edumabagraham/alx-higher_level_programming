7aeeb2d15fbd539e67240c2911e58a6a6938ad19
edumabagraham <edumabagrahamp@gmail.com>
2022-12-20 05:09:55 +0000

    Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,6 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except IndexError:
             print("out of range")
+            result = 0
         finally:
             new_arr.append(result)
     return new_arr
