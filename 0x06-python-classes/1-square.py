5e43245b05a088c854d32779245782ef601a3298
edumabagraham <edumabagrahamp@gmail.com>
2022-12-23 14:48:40 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    A class used to represent a Square
+
+    ...
+    Attributes
+    ----------
+    Private
+    -------
+    size: int
+    the size of the square
+    """
+
+    def __init__(self, size):
+        self.__size = size
